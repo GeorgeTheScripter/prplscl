5b31625c8ffd8966beb31c9ea7a94c6a07bd0262
GeorgeTheScripter <albegoff.georgy@yandex.ru>
2025-09-07 13:28:38 +0300

    Button

---
+++ src/components/Button.vue
@@ -0,0 +1,31 @@
+<template>
+  <button class="button">
+    <div>
+      <slot name="icon" />
+    </div>
+    <slot />
+  </button>
+</template>
+
+<script setup></script>
+
+<style scoped>
+.button {
+  display: flex;
+  gap: 10px;
+  border-radius: 10px;
+  padding: 14px;
+  background: var(--gradient);
+  border: none;
+  color: var(--color-primary);
+  font-family: var(--font);
+  font-weight: 600;
+  font-size: 20px;
+  cursor: pointer;
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background: var(--gradient-inverted);
+  }
+}
+</style>
